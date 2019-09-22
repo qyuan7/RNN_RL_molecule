37b276d823599d0ff6b6b4abed3dfb03dd327ba0
qyuan7 <qyuan@ic.ac.uk>
2019-09-22 11:04:10 +0100

    Added pading functions in data_structs

--- data_structs.py
+++ data_structs.py
@@ -5,48 +5,44 @@ import pickle
 from rdkit import Chem
 import sys
 import time
+import math
 import torch
 from torch.utils.data import Dataset
-import deepsmiles
 
 from utils import Variable
 
-
 class Vocabulary(object):
-    """
-    A class for handling encoding/decoding from SMILES to an array of indices
-    """
+    """A class for handling encoding/decoding from SMILES to an array of indices"""
     def __init__(self, init_from_file=None, max_length=140):
         self.special_tokens = ['EOS', 'GO']
         self.additional_chars = set()
         self.chars = self.special_tokens
         self.vocab_size = len(self.chars)
         self.vocab = dict(zip(self.chars, range(len(self.chars))))
-        self.reversed_vocab = {v : k for k, v in self.vocab.items()}
+        self.reversed_vocab = {v: k for k, v in self.vocab.items()}
         self.max_length = max_length
-        if init_from_file:
-            self.init_from_file(init_from_file)
+        if init_from_file: self.init_from_file(init_from_file)
 
     def encode(self, char_list):
-        """Takes a list of chars (eg '[NH]' and encodes to array of indices)"""
+        """Takes a list of characters (eg '[NH]') and encodes to array of indices"""
         smiles_matrix = np.zeros(len(char_list), dtype=np.float32)
         for i, char in enumerate(char_list):
             smiles_matrix[i] = self.vocab[char]
+
         return smiles_matrix
 
     def decode(self, matrix):
         """Takes an array of indices and returns the corresponding SMILES"""
         chars = []
         for i in matrix:
-            if i == self.vocab['EOS']:
-                break
+            if i == self.vocab['EOS']: break
             chars.append(self.reversed_vocab[i])
-        smiles = ''.join(chars)
-        smiles = smiles.replace('L', 'Cl').replace('R', 'Br')
+        smiles = "".join(chars)
+        smiles = smiles.replace("L", "Cl").replace("R", "Br")
         return smiles
 
     def tokenize(self, smiles):
-        """Takes a SMILES and return a list of chars/tokens"""
+        """Takes a SMILES and return a list of characters/tokens"""
         regex = '(\[[^\[\]]{1,6}\])'
         smiles = replace_halogen(smiles)
         char_list = re.split(regex, smiles)
@@ -61,7 +57,7 @@ class Vocabulary(object):
         return tokenized
 
     def add_characters(self, chars):
-        """Adds chars to the vocabulary"""
+        """Adds characters to the vocabulary"""
         for char in chars:
             self.additional_chars.add(char)
         char_list = list(self.additional_chars)
@@ -83,21 +79,20 @@ class Vocabulary(object):
     def __str__(self):
         return "Vocabulary containing {} tokens: {}".format(len(self), self.chars)
 
-
 class MolData(Dataset):
-    """
-    Custom Pytorch dataset that takes a file containing SMILES.
+    """Custom PyTorch Dataset that takes a file containing SMILES.
 
-    Args:
-        fname: path to a file containing \n separated SMILES
-        voc: a Vocabulary instance
-    Returns:
-        A custom PyTorch dataset for training the Piror
+        Args:
+                fname : path to a file containing \n separated SMILES.
+                voc   : a Vocabulary instance
+
+        Returns:
+                A custom PyTorch dataset for training the Prior.
     """
     def __init__(self, fname, voc):
         self.voc = voc
         self.smiles = []
-        with open(fname, 'r') as f:
+        with open(fname, 'r',encoding='utf-8-sig') as f:
             for line in f:
                 self.smiles.append(line.split()[0])
 
@@ -105,13 +100,14 @@ class MolData(Dataset):
         mol = self.smiles[i]
         tokenized = self.voc.tokenize(mol)
         encoded = self.voc.encode(tokenized)
-        return Variable(encoded)
+        if encoded is not None:
+            return Variable(encoded)
 
     def __len__(self):
         return len(self.smiles)
 
     def __str__(self):
-        return "Dataset containing {} structures".format(len(self))
+        return "Dataset containing {} structures.".format(len(self))
 
     @classmethod
     def collate_fn(cls, arr):
@@ -122,6 +118,81 @@ class MolData(Dataset):
             collated_arr[i, :seq.size(0)] = seq
         return collated_arr
 
+class Experience(object):
+    """Class for prioritized experience replay that remembers the highest scored sequences
+       seen and samples from them with probabilities relative to their scores."""
+    def __init__(self, voc, max_size=100):
+        self.memory = []
+        self.max_size = max_size
+        self.voc = voc
+
+    def add_experience(self, experience):
+        """Experience should be a list of (smiles, score, prior likelihood) tuples"""
+        self.memory.extend(experience)
+        if len(self.memory)>self.max_size:
+            # Remove duplicates
+            idxs, smiles = [], []
+            for i, exp in enumerate(self.memory):
+                if exp[0] not in smiles:
+                    idxs.append(i)
+                    smiles.append(exp[0])
+            self.memory = [self.memory[idx] for idx in idxs]
+            # Retain highest scores
+            self.memory.sort(key = lambda x: x[1], reverse=True)
+            self.memory = self.memory[:self.max_size]
+            print("\nBest score in memory: {:.2f}".format(self.memory[0][1]))
+
+    def sample(self, n):
+        """Sample a batch size n of experience"""
+        if len(self.memory)<n:
+            raise IndexError('Size of memory ({}) is less than requested sample ({})'.format(len(self), n))
+        else:
+            scores = [x[1] for x in self.memory]
+            sample = np.random.choice(len(self), size=n, replace=False, p=scores/np.sum(scores))
+            sample = [self.memory[i] for i in sample]
+            smiles = [x[0] for x in sample]
+            scores = [x[1] for x in sample]
+            prior_likelihood = [x[2] for x in sample]
+        tokenized = [self.voc.tokenize(smile) for smile in smiles]
+        encoded = [Variable(self.voc.encode(tokenized_i)) for tokenized_i in tokenized]
+        encoded = MolData.collate_fn(encoded)
+        return encoded, np.array(scores), np.array(prior_likelihood)
+
+    def initiate_from_file(self, fname, scoring_function, Prior):
+        """Adds experience from a file with SMILES
+           Needs a scoring function and an RNN to score the sequences.
+           Using this feature means that the learning can be very biased
+           and is typically advised against."""
+        with open(fname, 'r') as f:
+            smiles = []
+            for line in f:
+                smile = line.split()[0]
+                if Chem.MolFromSmiles(smile):
+                    smiles.append(smile)
+        scores = scoring_function(smiles)
+        tokenized = [self.voc.tokenize(smile) for smile in smiles]
+        encoded = [Variable(self.voc.encode(tokenized_i)) for tokenized_i in tokenized]
+        encoded = MolData.collate_fn(encoded)
+        prior_likelihood, _ = Prior.likelihood(encoded.long())
+        prior_likelihood = prior_likelihood.data.cpu().numpy()
+        new_experience = zip(smiles, scores, prior_likelihood)
+        self.add_experience(new_experience)
+
+    def print_memory(self, path):
+        """Prints the memory."""
+        print("\n" + "*" * 80 + "\n")
+        print("         Best recorded SMILES: \n")
+        print("Score     Prior log P     SMILES\n")
+        with open(path, 'w') as f:
+            f.write("SMILES Score PriorLogP\n")
+            for i, exp in enumerate(self.memory[:100]):
+                if i < 50:
+                    print("{:4.2f}   {:6.2f}        {}".format(exp[1], exp[2], exp[0]))
+                    f.write("{} {:4.2f} {:6.2f}\n".format(*exp))
+        print("\n" + "*" * 80 + "\n")
+
+    def __len__(self):
+        return len(self.memory)
 
 def replace_halogen(string):
     """Regex to replace Br and Cl with single letters"""
@@ -129,12 +200,13 @@ def replace_halogen(string):
     cl = re.compile('Cl')
     string = br.sub('R', string)
     string = cl.sub('L', string)
-    return string
 
+    return string
 
 def tokenize(smiles):
     """Takes a SMILES string and returns a list of tokens.
-       This will swap CL and Br to L and R and treat [xx] as one token"""
+    This will swap 'Cl' and 'Br' to 'L' and 'R' and treat
+    '[xx]' as one token."""
     regex = '(\[[^\[\]]{1,6}\])'
     smiles = replace_halogen(smiles)
     char_list = re.split(regex, smiles)
@@ -148,84 +220,36 @@ def tokenize(smiles):
     tokenized.append('EOS')
     return tokenized
 
-
-def canonicalize_smiles_from(fname):
-    """Reads a SMILE file and returns a list of RDKIT SMILES"""
+def canonicalize_smiles_from_file(fname):
+    """Reads a SMILES file and returns a list of RDKIT SMILES"""
     with open(fname, 'r') as f:
         smiles_list = []
         for i, line in enumerate(f):
+            if i % 100000 == 0:
+                print("{} lines processed.".format(i))
             smiles = line.split(" ")[0]
             mol = Chem.MolFromSmiles(smiles)
-            #if filter_mol(mol): #Don't filter mol for donor-acceptors since heavy atoms with varied elements exhist.
-            try:
-                smi = Chem.MolToSmiles(mol)
+            if filter_mol(mol):
                 smiles_list.append(Chem.MolToSmiles(mol))
-            except:
-                print(smiles)
-                continue
-            if (i+1) % 100000 == 0:
-                print("{} lines processed".format(i+1))
-
-                print("{} SMILES retrieved".format(len(smiles_list)))
+        print("{} SMILES retrieved".format(len(smiles_list)))
         return smiles_list
 
-
-def convert_to_deepsmile(smiles_list):
-    """
-    Convert smiles list to deepsmiles list. Make sure the smiles list is canonical.
-    Args:
-        smiles_list:
-
-    Returns: deepsmiles list
-
-    """
-    converter = deepsmiles.Converter(rings=True, branches=True)
-    deep_lst = [converter.encode(smi) for smi in smiles_list]
-    decoded_lst = []
-    final_deep_lst = []
-    num_decode, num_recover = 0, 0
-    for i in range(len(deep_lst)):
-        try:
-            decoded = converter.decode(deep_lst[i])
-
-        except deepsmiles.DecodeError as e:
-            decoded = None
-            print("DecodeError! Error message was {}".format(e.message))
-        decoded_lst.append(decoded)
-        if decoded:
-            num_decode += 1
-    decoded_can_lst = []
-    for item in decoded_lst:
-        if Chem.MolFromSmiles(item) is not None:
-            can_item = Chem.MolToSmiles(Chem.MolFromSmiles(item))
-            decoded_can_lst.append(can_item)
-        else:
-            decoded_can_lst.append(None)
-
-    for i in range(len(smiles_list)):
-        if smiles_list[i] == decoded_can_lst[i]:
-            final_deep_lst.append(deep_lst[i])
-    return final_deep_lst
-
-
-def filter_mol(mol, max_heavy_atom=50, min_heavy_atom=10, element_list=[6,7,8,9,16,17,35,14,34]):
+def filter_mol(mol, max_heavy_atoms=50, min_heavy_atoms=10, element_list=[6,7,8,9,16,17,35,33,51]):
     """Filters molecules on number of heavy atoms and atom types"""
     if mol is not None:
-        #num_heavy = min_heavy_atom<mol.GetNumHeavyAtoms()<max_heavy_atom
+        num_heavy = min_heavy_atoms<mol.GetNumHeavyAtoms()<max_heavy_atoms
         elements = all([atom.GetAtomicNum() in element_list for atom in mol.GetAtoms()])
-        #if num_heavy and elements:
-        if elements:
+        #if num_heavy and elements: remove the limit of elementlist for donor-acceptors
+        if num_heavy:
             return True
         else:
             return False
 
-
 def write_smiles_to_file(smiles_list, fname):
-    "Write a list of SMILES to a file"
+    """Write a list of SMILES to a file."""
     with open(fname, 'w') as f:
         for smiles in smiles_list:
             f.write(smiles + "\n")
-
 
 def filter_on_chars(smiles_list, chars):
     """Filters SMILES on the characters they contain.
@@ -238,24 +262,24 @@ def filter_on_chars(smiles_list, chars):
             smiles_list_valid.append(smiles)
     return smiles_list_valid
 
-
 def filter_file_on_chars(smiles_fname, voc_fname):
-    """Filters SMILES file using a vocabulary file.
-       Only SMILES containing nothing but the characters in the vocabulary will be retained."""
+    """Filters a SMILES file using a vocabulary file.
+       Only SMILES containing nothing but the characters
+       in the vocabulary will be retained."""
     smiles = []
     with open(smiles_fname, 'r') as f:
         for line in f:
             smiles.append(line.split()[0])
     print(smiles[:10])
+    chars = []
     with open(voc_fname, 'r') as f:
         for line in f:
-            chars.append(line.split([0]))
+            chars.append(line.split()[0])
     print(chars)
     valid_smiles = filter_on_chars(smiles, chars)
-    with open(smiles_fname, 'w') as f:
+    with open(smiles_fname + "_filtered", 'w') as f:
         for smiles in valid_smiles:
-            f.write(smiles + '\n')
-
+            f.write(smiles + "\n")
 
 def combine_voc_from_files(fnames):
     """Combine two vocabularies"""
@@ -264,10 +288,9 @@ def combine_voc_from_files(fnames):
         with open(fname, 'r') as f:
             for line in f:
                 chars.add(line.split()[0])
-    with open('_'.join(fnames) + '_combined', 'w') as f:
+    with open("_".join(fnames) + '_combined', 'w') as f:
         for char in chars:
-            f.write(char + '\n')
-
+            f.write(char + "\n")
 
 def construct_vocabulary(smiles_list):
     """Returns all the characters present in a SMILES file.
@@ -283,21 +306,62 @@ def construct_vocabulary(smiles_list):
             else:
                 chars = [unit for unit in char]
                 [add_chars.add(unit) for unit in chars]
+
     print("Number of characters: {}".format(len(add_chars)))
-    with open('data/Voc_cep', 'w') as f:
+    with open('data/Voc_danish', 'w') as f:
         for char in add_chars:
-            f.write(char + '\n')
+            f.write(char + "\n")
     return add_chars
 
+def can_smi_file(fname):
+    """
+
+    Args:
+        fname:
+
+    Returns:
+
+    """
+    out = open(fname+'cano', 'w')
+    with open (fname) as f:
+        for line in f:
+            smi = line.rstrip()
+            can_smi = Chem.MolToSmiles(Chem.MolFromSmiles(smi))
+            out.write(can_smi + '\n')
+    out.close()
+
+
+def batch_iter(data, batch_size=128, shuffle=True):
+    batch_num = math.ceil(len(data)/batch_size)
+    idx_arr = list(range(len(data)))
+    if shuffle:
+        np.random.shuffle(idx_arr)
+    for i in range(batch_num):
+        indices= idx_arr[i*batch_size: (i+1)*batch_size]
+        examples = [data[idx] for idx in indices]
+        examples = sorted(examples, key=lambda e:len(e), reverse=True)
+        yield i, examples
+
+
+def pad_seq(seqs):
+    batch_size = len(seqs)
+    seq_lengths = torch.LongTensor(list(map(len,seqs)))
+    max_length = len(seqs[0])
+    pad_seq = torch.zeros(batch_size, max_length,dtype=torch.long)
+    for i, seq in enumerate(seqs):
+        pad_seq[i, :len(seq)] = seq
+    return seq_lengths,pad_seq
+
+def mask_seq(seqs, seq_lens):
+    mask = torch.zeros(seqs.size(0),seqs.size(1))
+    for i, length in enumerate(seq_lens):
+        mask[i, 0:length] = seqs[i, 0:length]
+    return mask
 
 if __name__ == "__main__":
     smiles_file = sys.argv[1]
     print("Reading smiles...")
-    smiles_list = canonicalize_smiles_from(smiles_file)
-    #deep_lst = convert_to_deepsmile(smiles_list)
-    print('Constructing vocabulary...')
+    smiles_list = canonicalize_smiles_from_file(smiles_file)
+    print("Constructing vocabulary...")
     voc_chars = construct_vocabulary(smiles_list)
-    write_smiles_to_file(smiles_list, "data/CEP_cano.smi")
-
-
-
+    write_smiles_to_file(smiles_list, "data/danish.smi")
